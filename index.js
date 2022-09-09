4eb69793f8d4ccaed656cf8d7afa3be3d5d4b2f8
canasmh <canasmh@yahoo.com>
2022-09-09 16:11:28 -0400

    Complete the final task

--- index.js
+++ index.js
@@ -162,8 +162,9 @@ Use the getLastReview function below to do the following:
 */
 
 
-function getLastReview(/*Your code here*/) {
-  /*Your code here*/
+function getLastReview(reviews) {
+  let review = reviews[reviews.length - 1];
+  return `${review.name} gave the restaurant a ${review.rating} star review, and their feedback was: ${review.feedback}`;
 } 
 
 
